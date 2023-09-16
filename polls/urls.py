0bfb37a0e028462ddbb3774bc8f1659263a1fddc
Jahongir <dodajoy0777@gmail.com>
2023-09-16 07:49:10 +0500

    last

--- polls/urls.py
+++ polls/urls.py
@@ -1,8 +1,27 @@
 from django.urls import path
-from .views import getwater, postwater, detail
+
+from .views import GetAllWater, PostWater, GetDetailWater, PatchWater, DeleteWater, AllFunctionWater, PostGetWater
+
+
+from .views import GetAllKarta, GetDetailKarta, PostKarta, PatchKarta, DeleteKarta, AllFunctionKarta, PostGetKarta
 
 urlpatterns=[
-    path('all/', getwater.as_view()),
-    path('detail/<int:yourid>', detail),
-    path('create/', postwater)
+        path('GetAllKarta/', GetAllKarta.as_view()),
+    path('GetDetailKarta/<int:pk>', GetDetailKarta.as_view()),
+    path('PostKarta/', PostKarta.as_view() ),
+    path('PatchKarta/<int:pk>', PatchKarta.as_view()),
+    path('DeleteKarta/<int:pk>', DeleteKarta.as_view()),
+    path('PostGetKarta/', PostGetKarta.as_view()),
+    path('AllFunctionKarta/<int:pk>', AllFunctionKarta.as_view())
+    ,
+    path('GetAllWater/', GetAllWater.as_view()),
+    path('GetDetailWater/<int:pk>', GetDetailWater.as_view()),
+    path('PostWater/', PostWater.as_view() ),
+    path('PatchWater/<int:pk>', PatchWater.as_view()),
+    path('DeleteWater/<int:pk>', DeleteWater.as_view()),
+    path('PostGetWater/', PostGetWater.as_view()),
+    path('AllFunctionWater/<int:pk>', AllFunctionWater.as_view())
+
+
+
 ]
--- polls/views.py
+++ polls/views.py
@@ -4,6 +4,7 @@ from django.http import JsonResponse
 from .serializer import waterSerializer, kartaSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import generics
 
 # Create your views here.
 
@@ -17,24 +18,113 @@ from rest_framework.response import Response
 #     #     })
 #     return JsonResponse(result.data, safe=False)
 
-class getwater(APIView):
-    def get(self, request):
-        kalla = water.objects.all()
-        barakalla=waterSerializer(kalla, many=True)
-        return Response(barakalla.data)
+# class getwater(APIView):
+#     def get(self, request):
+#         kalla = water.objects.all()
+#         barakalla=waterSerializer(kalla, many=True)
+#         return Response(barakalla.data)
+class GetAllWater(generics.ListAPIView):
+    queryset=water.objects.all()
+    serializer_class=waterSerializer
+
+
+
+
+class GetDetailWater(generics.RetrieveAPIView):
+    queryset = water.objects.all()
+
+    serializer_class=waterSerializer
+
+class PostWater(generics.CreateAPIView):
+
+
+
+    queryset=water.objects.all()
+    serializer_class=waterSerializer
+
+class PatchWater(generics.UpdateAPIView):
+    queryset=water.objects.all()
+    serializer_class=waterSerializer
+
+
+
+
+
+# def detail(request, yourid):
+#     some = karta.objects.filter(id=myid)
+#     forgett = kartaSerializer(some, many=True)
+#     return JsonResponse(forgett.data, safe=False)
+
+
+
+class DeleteWater(generics.DestroyAPIView):
+    queryset=water.objects.all()
+    serializer_class=waterSerializer
+
+class PostGetWater(generics.ListCreateAPIView):
+    queryset=water.objects.all()
+    serializer_class=waterSerializer
+
+class AllFunctionWater(generics.RetrieveUpdateDestroyAPIView):
+    queryset=water.objects.all()
+    serializer_class=waterSerializer
+
+
     
 
 
-def detail(request, yourid):
-    some = karta.objects.filter(id=myid)
-    forgett = kartaSerializer(some, many=True)
-    return JsonResponse(forgett.data, safe=False)
+# def detail(request, yourid):
+#     some = karta.objects.filter(id=myid)
+#     forgett = kartaSerializer(some, many=True)
+#     return JsonResponse(forgett.data, safe=False)
 
-class postwater(APIView):
-    def post(self, request):
-        main_body=request.data
-        sss = waterSerializer(data=main_body)
-        if sss.is_valid():
-            sss.save()
-            return Response(sss.data)
-        return Response(sss.errors)
+# class postwater(APIView):
+#     def post(self, request):
+#         main_body=request.data
+#         sss = waterSerializer(data=main_body)
+#         if sss.is_valid():
+#             sss.save()
+#             return Response(sss.data)
+#         return Response(sss.errors)
+
+
+class GetAllKarta(generics.ListAPIView):
+    queryset=karta.objects.all()
+    serializer_class=kartaSerializer
+
+class GetDetailKarta(generics.RetrieveAPIView):
+    queryset = karta.objects.all()
+    serializer_class=kartaSerializer
+
+
+class AllFunctionKarta(generics.RetrieveUpdateDestroyAPIView):
+    queryset=karta.objects.all()
+    serializer_class=kartaSerializer
+
+class PostKarta(generics.CreateAPIView):
+    queryset=karta.objects.all()
+    serializer_class=kartaSerializer
+# class postwater(APIView):
+#     def post(self, request):
+#         main_body=request.data
+#         sss = waterSerializer(data=main_body)
+#         if sss.is_valid():
+#             sss.save()
+#             return Response(sss.data)
+#         return Response(sss.errors)
+
+class PatchKarta(generics.UpdateAPIView):
+    queryset=karta.objects.all()
+    serializer_class=kartaSerializer
+
+class DeleteKarta(generics.DestroyAPIView):
+    queryset=karta.objects.all()
+    serializer_class=kartaSerializer
+
+
+
+class PostGetKarta(generics.ListCreateAPIView):
+    queryset=karta.objects.all()
+    serializer_class=kartaSerializer
+
+
