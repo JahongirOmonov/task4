8edbcb5f53d72e3fe77de3dfc0fd0e27745925ee
Jahongir <dodajoy0777@gmail.com>
2023-09-18 16:09:40 +0500

    last

--- polls/models.py
+++ polls/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -9,6 +10,7 @@ from django.db import models
 class water(models.Model):
     name = models.CharField(max_length=200, default='')
     date_of_invent = models.DateField(default=datetime.now)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     def __str__(self) -> str:
         return self.name
 
@@ -16,5 +18,6 @@ class water(models.Model):
 class karta(models.Model):
     comName = models.CharField(max_length=201, default='')
     number = models.PositiveIntegerField(default=1)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     def __str__(self) -> str:
         return self.comName
--- polls/serializer.py
+++ polls/serializer.py
@@ -1,17 +1,27 @@
 from rest_framework import serializers
 from .models import water, karta
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User 
 
 
 class waterSerializer(serializers.ModelSerializer):
     class Meta:
         model=water
-        fields = ('name', 'date_of_invent')
+        fields = ('name', 'date_of_invent', 'user')
+
+    def create(self, validated_data):
+        validated_data['user']=get_object_or_404(User, username=self.context['request'].user)
+        return super(waterSerializer, self).create(validated_data)
 
 
 class kartaSerializer(serializers.ModelSerializer):
     class Meta:
         model=karta
-        fields=('comName', 'number')
+        fields=('comName', 'number', 'user')
+
+    def create(self, validated_data):
+        validated_data['user']=get_object_or_404(User, username=self.context['request'].user)
+        return super(kartaSerializer, self).create(validated_data)
 
 
 
